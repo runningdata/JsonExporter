b8d22375d3e285cd08465dcd598892d70693030a
WillCup <willcup@163.com>
2018-05-09 10:58:48 +0800

    fix yarn

--- settings.py
+++ settings.py
@@ -1,6 +1,6 @@
 import os
 
-YARN = 'http://10.2.19.83:8088/ws/v1/cluster/apps'
+YARN = 'http://yarn.com:8088/ws/v1/cluster/apps'
 APP_PATTERN = '(.*)_monitor_online'
 
 DB_HOST = os.environ.get('DB_HOST')
